7261f49985e47f2bd280ce8df0d47bfa1125ad7e
flewis21 <fabian.lewis@outlook.com>
2021-09-20 17:31:16 -0400

    update nodejs

--- handler.js
+++ handler.js
@@ -1,7 +1,9 @@
-'use strict';
++'use strict';
 
 module.exports.helloWorld = (event, context, callback) => {
   const response = {
+  	WriteHead: (404, { 'Content-Type': 'text/plain'}),
+  	Write: ('Error 404: Resource not found.'),
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
@@ -9,8 +11,30 @@ module.exports.helloWorld = (event, context, callback) => {
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
-    }),
-  };
+	}),
+ };
 
-  callback(null, response);
+ callback(null, response) = {
+ 
+	if (req.Method == 'GET' && req.url == '/') {
+		res.WriteHead(200, { 'content-type': 'text/html' });
+		fs.createReadStream('./complete-javascript-course/01-Fundamentals-Part-1/starter/index.html').pipe(res);
+	} else {
+		send404(res);
+	}
+ };
 };
+console.log('server running on port 4000');
+	
+var nodegit = require('nodegit'),
+    path = require('path');
+
+var url = "",
+    local = "",
+    cloneOpts = {};
+
+nodegit.Clone(url, local, cloneOpts).then(function (repo) {
+    console.log("Cloned " + path.basename(url) + "to  " + repo.workdir());
+}).catch(function (err) {
+    console.log(err);
+});
