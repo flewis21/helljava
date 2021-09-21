10647573cb2ba063667cf996c514021a3f7532b6
wildstage <wildstage@dangercombos.com>
2021-09-21 22:31:28 +0000

    node.js callbacks

--- handler.js
+++ handler.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
-const data = fs.readFileSync('./complete-javascript-course/01-Fundamentals-Part-1/index.html')
-console.log(data.toString())
+fs.readFile('./complete-javascript-course/01-Fundamentals-Part-1/starter/index.html', (err, data) => {
+  if (err) return console.error(err)
+  console.log(data.toString())
+})
 console.log('Program Ended')
